ff5a3bc9e3133464ef36aeb63e15971b9e673a14
nicefeiniu <872938058@qq.com>
2018-04-11 22:16:02 +0800

    修改数据库部分代码

--- air_ticket/data.py
+++ air_ticket/data.py
@@ -2,53 +2,47 @@ import time, sys
 import configparser
 import pymysql
 
+'''
+   * 日期时间，int型（date, depTime, arrTime, seaTime）
+   * 如 2018041018
+   * 后期好依据这个排序
+'''
 class Data:
-    def __init__(self, **info):
-        '''
-        * 日期时间，int型（date, depTime, arrTime, seaTime）
-        * 如 2018041018
-        * 后期好依据这个排序
-        '''
-        self.depAir = info['depAir']
-        self.arrAir = info['arrAir']
-        self.minPrice = int(info['minPrice'])
-        self.code = info['code']
-        self.date = int(info['date'])
-        self.depTime = int(info['depTime'])
-        self.arrTime = int(info['arrTime'])
-        self.seaTime = int(time.strftime('%Y%m%d%H',time.localtime()))
-
     def __Connect(self):
         conf = configparser.ConfigParser()
-        conf.read('config.ini')
+        try:
+            conf.read('config.ini')
+        except Exception as err:
+            print('ERROR CONFIG: %s' % err)
+            sys.exit(1)
         conf = conf['MYSQL']
         # 链接mysql数据库，使用pymysql
         try:
             self.conn = pymysql.connect(host=conf['Host'], user=conf['UserName'], password=conf['PassWord'], db=conf['DataBase'], port=int(conf['Port']))
-            cur = self.conn.cursor()
+            self.cur = self.conn.cursor()
         except  Exception as err:
             print("ERROR CONNECT: %s" % err)
             sys.exit(1)
-        return cur
+        return self.cur
 
-    def insert_db(self):
+    def insert_db(self,code: str, depAir: str, arrAir: str, minPrice: int, date: int, depTime: int, arrTime: int, seaTime: int):
         # print(type(conf['Port']))
-        sql_insert = "INSERT INTO `ticket_prices`.`qunar_min_prices` (`aircode`, `depair`, `arrair`, `deptime`, `arrtime`, `date`, `seardate`, `minprice`) VALUES('{0.code}', '{0.depAir}', '{0.arrAir}', '{0.depTime}', '{0.arrTime}', '{0.date}', '{0.seaTime}', '{0.minPrice}');".format(self)
+        sql_insert = "INSERT INTO `ticket_prices`.`qunar_min_prices` (`aircode`, `depair`, `arrair`, `deptime`, `arrtime`, `date`, `seardate`, `minprice`) VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');" % (code,depAir,arrAir,depTime,arrTime,date,seaTime,minPrice)
         print(sql_insert)
         try:
             cur = self.__Connect()
             cur.execute(sql_insert)
-            cur.close()
             self.conn.commit()
-            self.conn.close()
         except Exception as err:
             print("ERROR INSERT: %s" % err)
             sys.exit(1)
+        finally:
+            cur.close()
+            self.conn.close()
 
 
 
-
-d = Data(depAir='PNY', arrAir='PGV', minPrice=875, code='MU5431', date=20180410, depTime=2135, arrTime=2335, seaTime=2018041021)
-d.insert_db()
+d = Data()
+d.insert_db('CA1254','PVG','CNG',456,20180411,2244,2344,2018041121)
 
 
