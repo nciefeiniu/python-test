f5eded861012270c1f8c068df3d4bb8bbfb14442
nicefeiniu <872938058@qq.com>
2018-05-29 15:43:29 +0800

    装饰器

--- decorator/decorator1.py
+++ decorator/decorator1.py
@@ -3,19 +3,25 @@
 
 import time
 
-def timer(func):
-    def warpper(*arg, **kwargs):
-        start_time = time.time()
-        print()
-        func()
-        stop_time = time.time()
-        print('这个方法运行了：%s' % (stop_time - start_time))
-    return warpper
+def timer(auth_type):
+    def out_warpper(func):
+        print(auth_type)
+        def warpper(*args, **kwargs):
+            if auth_type == 'lamp':
+                start_time = time.time()
+                print(args)
+                func(*args,  **kwargs)
+                stop_time = time.time()
+                print('这个方法运行了：%s' % (stop_time - start_time))
+            elif auth_type == 'lnmp':
+                print('NONONONO')
+        return warpper
+    return out_warpper
 
-@timer
+@timer(auth_type='lnmp')
 def test1(name):
     time.sleep(3)
-    print('测试')
+    print('测试员： %s' % name)
 
 
 test1('liutao')
