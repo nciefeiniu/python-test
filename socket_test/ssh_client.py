0e7282d204bf3eb2de46483cf2b32f1453d90bfa
nicefeiniu <872938058@qq.com>
2018-06-06 19:51:09 +0800

    完善一点功能

--- socket_test/ssh_client.py
+++ socket_test/ssh_client.py
@@ -9,5 +9,12 @@ while True:
         while True:
             msg = input('>>>>').strip()
             sshclient.send(msg.encode())
-            data = sshclient.recv(1024)
-            print(data.decode('utf-8'))
+            # 返回命令结果长度
+            res_size = sshclient.recv(1024)
+            print(res_size)
+            res = b''
+            recevied_size = 0
+            while recevied_size < int(res_size):
+                res += sshclient.recv(1024)
+                recevied_size = len(res)
+            print(res.decode())
--- socket_test/ssh_server.py
+++ socket_test/ssh_server.py
@@ -3,18 +3,38 @@
 
 import socket
 import os
+from datetime import datetime
 
-while True:
-    with socket.socket() as sshserver:
-        sshserver.bind(('127.0.0.1', 1478))
-        sshserver.listen()
-        conn,addr = sshserver.accept()
+def write_logs(func):
+    def wrapper(*args, **kwargs):
+        msg = func.__name__ + ' is begining at   ' + (datetime.now()).__format__('%Y-%m-%d %H')+ '\r\n'
+        print(msg)
+        with open('sshlogs.txt', 'at') as f:
+            f.write(msg)
+        func(*args, **kwargs)
+        # msg2 = func.__name__ + ' closed at   ' + (datetime.now()).__format__('%Y-%m-%d %H')+ '\r\n'
+        # with open('sshlogs.txt', 'at') as f:
+        #     f.write(msg2)
+    return wrapper
 
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                print('None')
-                continue
-            print('Message:', data.decode('utf-8'))
-            msg = os.popen(data.decode('utf-8')).read()
-            conn.send(msg.encode('utf-8'))
+@write_logs
+def ssh_server():
+    print('>>>begining')
+    while True:
+        with socket.socket() as sshserver:
+            sshserver.bind(('127.0.0.1', 1478))
+            sshserver.listen()
+            while True:
+                conn, addr = sshserver.accept()
+                data = conn.recv(1024)
+                if not data:
+                    print('None')
+                    continue
+                print('Message:', data.decode('utf-8'))
+                msg = os.popen(data.decode('utf-8')).read()
+                conn.send(str(len(msg.encode('utf-8'))).encode('utf-8'))
+                conn.sendall(msg.encode('utf-8'))
+                print('send over')
+
+if __name__ == "__main__":
+    ssh_server()
