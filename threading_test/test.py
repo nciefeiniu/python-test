49e309e95dbd0c02f4e9ae039739ea2aba3c1e3c
nicefeiniu <872938058@qq.com>
2018-06-15 08:29:29 +0800

    多线程联系

--- threading_test/test.py
+++ threading_test/test.py
@@ -1,2 +1,40 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
+
+def getxin(x_num:int, x_lists:list, q_num:int, x_site:list):
+    result = []
+    for i in x_site:
+        num = 0
+        r_min = i[0]
+        r_max = i[2]
+        c_min = i[1]
+        c_max = i[3]
+        print(r_min,r_max,c_min,c_max)
+        for j in x_lists:
+            if j[0] >= r_min and j[0] <= r_max:
+                if j[1] >= c_min and j[1] <= c_max:
+                    print(j)
+                    num += 1
+        print('==============')
+        result.append(num)
+    return result
+
+
+if __name__ == '__main__':
+    # x_num = input('输入星星个数：').strip()
+    # x_lists = []
+    # for i in range(int(x_num)):
+    #     site = input('请输入星星第%s个坐标(如：1 1)' % str(i + 1)).strip()
+    #     x_lists.append(site.split())
+    # q_num = input('请输入问题个数：').strip()
+    # x_site = []
+    # for i in range(int(q_num)):
+    #     site = input('请输入第%s个问题(如：1 1 2 2)' % str(i + 1)).strip()
+    #     x_site.append(site.split())
+
+    x_num = 4
+    x_lists = [[1,1], [2,2],[3,3],[1,3]]
+    q_num = 5
+    x_site = [[1,1,2,2],[1,1,3,3],[2,2,3,3],[1,2,2,3],[1,1,5,5]]
+    test = getxin(x_num,x_lists,q_num,x_site)
+    print(test)
--- threading_test/therad_object.py
+++ threading_test/therad_object.py
@@ -1,2 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
+
+import threading
+import time
+
+class MyThread(threading.Thread):
+    def __init__(self,name):
+        # 调用父类的构造方法
+        super(MyThread, self).__init__()
+        self.name = name
+
+    def run(self):
+        print('Threading %s is running' % self.name)
+        time.sleep(5)
+
+# # 这种方式无意义，相当于单线程
+# for i in range(50):
+#     t = MyThread('t-%s' % str(i+1))
+#     t.start()
+#     # 等待这个线程执行完毕，程序再继续往下走
+#     t.join()
+# print('over')
+
+# 并发，且主线程等待子线程结束再结束
+start_time = time.time()
+thread_lists = []
+for i in range(50):
+    t = MyThread('t-%s' %i)
+    t.start()
+    thread_lists.append(t)
+print(thread_lists)
+for m in thread_lists:
+    print(m)
+    m.join()
+print('over')
+print(time.time()-start_time)
