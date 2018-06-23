8c58fc2028907ed00451d842693c32b9c01e9d71
nicefeiniu <872938058@qq.com>
2018-06-23 14:14:06 +0800

    update Servers and Clients

--- IOmultiplexing/ftp/ftpclient.py
+++ IOmultiplexing/ftp/ftpclient.py
@@ -6,46 +6,73 @@ import json
 import os
 import sys
 
-def ftpclients(host, port):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-        try:
-            client.connect((host, port))
-        except Exception as e:
-            print("连接服务端失败,稍后再试")
-            print(e)
-            sys.exit(1)
-        while True:
-            print("需要输入完整路径~")
-            data = input('>>>:').strip()
-            cmd_info = data.split()
-            if cmd_info[0] == 'get':
-                filename = cmd_info[1]
-                msg = {
-                    "action": cmd_info[0],
-                    "filename": filename,
-                    "size": 0
-                }
-            elif cmd_info[0] == 'put':
-                filename = cmd_info[1].split('/')[-1]
-                msg = {
-                    "action": cmd_info[0],
-                    "filename": filename,
-                    "size": os.stat(cmd_info[1]).st_size
-                }
-            else:
-                continue
-            client.send(json.dumps(msg).encode('utf-8'))
-            # 接收服务端的返回数据
-            server_resp = client.recv(1024)
+class FtpClient:
+    def ftpclients(self, host, port):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+            try:
+                client.connect((host, port))
+            except Exception as e:
+                print("连接服务端失败,稍后再试")
+                print(e)
+                sys.exit(1)
+            while True:
+                print("需要输入完整路径~")
+                data = input('>>>:').strip()
+                cmd_info = data.split()
+                if cmd_info[0] == 'get':
+                    # 下载   get test.txt /home/test/    下载test.txt到/home/test/这个目录下
+                    filename = cmd_info[1]
+                    storage_dir = "/download/"
+                    if len(cmd_info) == 3:
+                        storage_dir = cmd_info[2]
+                    msg = {
+                        "action": cmd_info[0],
+                        "filename": filename,
+                        "size": 0
+                    }
+                    # 向服务器发送数据
+                    client.send(json.dumps(msg).encode('utf-8'))
+                    if hasattr(self, cmd_info[0]):
+                        func = getattr(self, cmd_info[0])
+                        func(client, filename, storage_dir)
+                elif cmd_info[0] == 'put':
+                    # 上传 put /home/test.txt
+                    filename = cmd_info[1].split('/')[-1]
+                    msg = {
+                        "action": cmd_info[0],
+                        "filename": filename,
+                        "size": os.stat(cmd_info[1]).st_size
+                    }
+                    client.send(json.dumps(msg).encode('utf-8'))
+                    # 接收服务端的返回数据
+                    server_resp = client.recv(1024)
+                else:
+                    continue
 
-def download(conn, ):
-    pass
 
-def upload():
-    pass
+    def get(self, conn, filename, dir):
+        # 接收服务端的返回数据
+        server_resp = conn.recv(1024)
+        data_info = json.loads(server_resp.decode('utf-8'))
+        # 给服务端确认，可以开始收取数据
+        msg = {
+            "size": data_info["size"]
+        }
+        conn.send(json.dumps(msg).encode('utf-8'))
+        cur_filesize = 0
+        with open(dir+filename, 'wb') as f:
+            while cur_filesize < data_info['size']:
+                data = conn.recv(1024)
+                f.write(data)
+                cur_filesize += len(data)
+        print('download done')
+
+    def put(self):
+        pass
 
 
 
 if __name__ == "__main__":
     HOST, PORT = '140.143.96.219', 9999
-    ftpclients(HOST, PORT)
+    ftp = FtpClient()
+    ftp.ftpclients(HOST, PORT)
--- IOmultiplexing/ftp/selectorsftpservers.py
+++ IOmultiplexing/ftp/selectorsftpservers.py
@@ -11,27 +11,34 @@ class ftpservers:
         self.host = host
         self.port = port
         self.sel = selectors.DefaultSelector()
-        self.rootdir = '/home/selftp/'
+        # 存放路径,私有变量
+        self.__rootdir = '/home/selftp/'
         self.createdir()
+        # 同一个conn，同时只有一个消息在字典里面
+        self.__message = {}
 
     def createdir(self):
-        if not os.path.exists(self.rootdir):
-            os.makedirs(self.rootdir, )
+        if not os.path.exists(self.__rootdir):
+            os.makedirs(self.__rootdir, )
 
     def servers(self):
-        with socket,socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-            server.bind((self.host,self.port))
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+            server.bind((self.host, self.port))
             server.setblocking(False)
-            server.listen()
+            server.listen(1000)
             # 有活跃的连接就交给helder这个方法处理
             self.sel.register(server,selectors.EVENT_READ, self.helder)
-
             while True:
-                events = self.sel.select()
-                for key, mask in events:
-                    callback = key.data
-                    callable(key.fileobj, mask)
+                try:
+                    events = self.sel.select()
+                    for key, mask in events:
+                        callback = key.data
+                        callback(key.fileobj, mask)
+                except Exception as e:
+                    print(e)
+                    continue
 
+    # 建立连接,注册EVENT_READ事件
     def helder(self, sock, mask):
         conn, addr = sock.accept()
         print(addr,' connection success ')
@@ -39,27 +46,64 @@ class ftpservers:
         self.sel.register(conn,selectors.EVENT_READ, self.actions)
 
     def actions(self, conn, mask):
+        # 先把之前注册的注销掉，后面重新注册新的事件
+        # self.sel.unregister(conn)
         # 把json转换为字典
         c_data = json.loads(conn.recv(1024).decode('utf-8'))
-        filename = c_data['filename']
-        if c_data['action'] == 'put':
-            self.upload(conn, filename)
-        elif c_data['action'] == 'get':
-            self.download(conn, filename)
+        if c_data:
+            filename = c_data['filename']
+            if c_data['action'] == 'put':
+                # 上传
+                print('准备上传')
+                self.__message[conn] = filename
+                self.sel.modify(conn, selectors.EVENT_READ, self.upload)
+            elif c_data['action'] == 'get':
+                print('准备下载')
+                # 下载,吧需要下载的文件名放在message字典中，用conn为key, filename为value
+                self.__message[conn] = filename
+                self.sel.modify(conn, selectors.EVENT_READ, self.download)
+                # 给客户端发送数据信息
+                if os.path.isfile(self.__rootdir + filename):
+                    msg = {
+                        "filename": filename,
+                        "size": os.stat(self.__rootdir + filename).st_size
+                    }
+                    # 给客户端发送文件信息,等待客户端回应再发送数据
+                    conn.send(json.dumps(msg).encode('utf-8'))
+        else:
+            print(conn, ' losing')
+            self.sel.unregister(conn)
+            conn.close()
 
-    def download(self, conn, filename):
-        if os.path.isfile(self.rootdir+filename):
-            msg = {
-                "filename": filename,
-                "size": os.stat(self.rootdir+filename).st_size
-            }
+    def download(self, conn, mask):
+        # 从客户端接收确认信息，开始传数据
+        client_response = conn.recv(1024)
+        print('开始下载')
+        filename = self.__message[conn]
+        if os.path.isfile(self.__rootdir+filename):
+            # msg = {
+            #     "filename": filename,
+            #     "size": os.stat(self.__rootdir+filename).st_size
+            # }
             # 给客户端发送文件信息,等待客户端回应再发送数据
-            conn.send(json.loads(msg).encode('utf-8'))
-            client_response = conn.recv(1024)
-            with open(self.rootdir+filename) as f:
-                for line in f:
-                    conn.send(1024)
-                print('send done')
+            # conn.send(json.dumps(msg).encode('utf-8'))
+            # 当客户端返回能接受数据了再开始传输数据
 
-    def upload(self):
+            with open(self.__rootdir+filename, 'rb') as f:
+                for line in f:
+                    conn.send(line)
+                print('send done')
+        # 删除字典中当前连接的消息, 以及注销事件
+        del self.__message[conn]
+        self.sel.modify(conn, selectors.EVENT_READ, self.actions)
+        # self.sel.unregister(conn)
+        # self.sel.register(conn, selectors.EVENT_READ, self.actions)
+
+    def upload(self, conn, mask):
         pass
+
+
+if __name__ == "__main__":
+    HOST, PORT = '127.0.0.1',9999
+    s = ftpservers(HOST, PORT)
+    s.servers()
