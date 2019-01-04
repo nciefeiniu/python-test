8f18617916cd9f1dbe887a5909c7e5a8c0ca9221
Liu Tao <tao.liu@epmap.org>
2019-01-04 09:03:01 +0800

    快排的python实现

--- suanfa/quick_sort.py
+++ suanfa/quick_sort.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# def quick_sort(array, left, right):
-#     if len(array) > 1:
-#         pass
-#
-#
-# def partition(array):
-#     x = array[-1]
-#     i = -1
-#     for j in range(len(array) - 1):
-#         if array[j] <= x:
-
 
 def quick_sort(array, left, rigth):
     if left < rigth:
@@ -27,10 +16,39 @@ def partition(array, l, r):
         if array[j] <= x:
             i += 1
             array[i], array[j] = array[j], array[i]
+        print array
     array[i + 1], array[r] = array[r], array[i + 1]
     return i + 1
 
 
-lists = [2,9,5,4,7,6,3,1]
-quick_sort(lists,0, len(lists) - 1)
-print(lists)
+def doit(lists, left, right):
+    _base_num = left
+    _base = lists[_base_num]
+    while right != left:
+        while right != left and lists[right] >= _base:
+            right -= 1
+        while right != left and lists[left] <= _base:
+            left += 1
+        # print left, right
+        lists[left], lists[right] = lists[right], lists[left]
+
+    lists[left], lists[_base_num] = lists[_base_num], lists[left]
+    print lists
+    return left
+
+
+def quick_sort2(inputs, l, r):
+    if l < r:
+        q = doit(inputs, l, r)
+        quick_sort2(inputs, l, q-1)
+        quick_sort(inputs, q+1, r)
+
+
+if __name__ == '__main__':
+    # lists = [2,9,5,9,4,7,1,5,4]
+    # quick_sort(lists,0, len(lists) - 1)
+    # print(lists)
+
+    lists2 = [1,1]
+    quick_sort2(lists2, 0, len(lists2) - 1)
+    print lists2
